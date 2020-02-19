c2aaced4923c225b5f41245482660d722664e0aa
xo28122000 <jainam.2000@yahoo.com>
2020-02-18 22:11:57 -0800

    project done!!!

--- Day1/colorpicker/src/App.js
+++ Day1/colorpicker/src/App.js
@@ -2,28 +2,48 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = { start: false, R: 0, G: 0, B: 0 };
+  state = {
+    start: false,
+    R: 0,
+    G: 0,
+    B: 0,
+    A: 0.9,
+    speed: 2,
+    instruction: "Click anywhere to start"
+  };
 
   render() {
     const sleep = seconds => {
       return new Promise(resolve => setTimeout(resolve, seconds * 60));
     };
+
     document.onclick = async e => {
       if (!this.state.start) {
-        this.setState({ start: true });
+        this.setState({ start: true, instruction: "Click anywhere to stop." });
         while (this.state.start) {
           this.setState({
             R: Math.random() * 256,
             G: Math.random() * 256,
-            B: Math.random() * 256
+            B: Math.random() * 256,
+            A: Math.random()
           });
-
-          await sleep(2);
+          await sleep(this.state.speed);
         }
       } else {
-        this.setState({ start: false });
+        this.setState({ start: false, instruction: "Click anywhere to start." });
       }
     };
+    const sliderChange = () => {
+      this.setState({
+        speed: document.getElementById("formControlRange").value
+      });
+    };
+
+    // const innerClicked = event => {
+    //   console.log("inner here");
+    //   event.stopPropagation();
+    // };
+
     return (
       <div
         id="Root"
@@ -35,15 +55,26 @@ class App extends Component {
             this.state.G +
             "," +
             this.state.B +
-            ",0.8)"
+            "," +
+            this.state.A +
+            ")"
         }}
       >
         <div id="inforow">
           <div className="container" style={{ paddingTop: "20px" }}>
-            <h4>
-              Click anywhere to start
-              <br /> Click again to stop
-            </h4>
+            {this.state.instruction}
+            <br />
+
+            <label htmlFor="formControlRange">Speed:</label>
+            <input
+              type="range"
+              className="form-control-range custRange"
+              id="formControlRange"
+              min="0"
+              max="10"
+              step="0.1"
+              onChange={sliderChange}
+            />
           </div>
         </div>
       </div>
